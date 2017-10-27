a35e722ff8eb38b59208b99e16f522877eb882f6
Michael Ortiz <ortiz_michael@bah.com>
2017-10-27 11:34:36 -0400

    functional version

--- script.js
+++ script.js
@@ -1,48 +1,79 @@
 // start hangman game!
-
+// $( document ).ready(function() {
 var wordArray=[]
 var word = prompt('Player 1, submit a word for Player 2 to guess!: ');
 wordArray.push(word)
+alert('Player 2, the secret word has ' + word.length + ' letters')
+const buttn=$('#button')
+const buttn1=$('#button1')
 
 
-// using above array we choose a random word.
+
 var chosenWord = wordArray[0];
+console.log(chosenWord)
 
 
-// global variables
+  // global variables
 
 var s;
 var count = 0;
 var answerArray = [];
 
 
-// filling the answer array with underscores as required
-// number of underscores correlates to the randomly selected word in the array
 
-
-
-function startUp() {
+function begin() {
   for (var i = 0; i < chosenWord.length; i++) {
     answerArray[i] = "_";
     document.write();
+    console.log(answerArray)
   }
 
-  // putting in a string
+    // putting in a string
   s = answerArray.join(" ");
-  document.getElementById("answer").innerHTML = s;
+  $('#answer').html = s;
 }
 
-function letter() {
-  var letter = document.getElementById("letter").value;
+buttn.on('click', letter)
+
+function letter(){
+  var letter = $('#letter').val();
 
   if (letter.length > 0) {
     for (var i = 0; i < chosenWord.length; i ++) {
       if (chosenWord[i] === letter) {
         answerArray[i] = letter;
+        console.log(answerArray.length)
       }
     }
-  }
+
+    console.log('here')
     count++;
-    document.getElementById("counter").innerHTML = "Number of clicks: " + count;
-    document.getElementById("answer").innerHTML = answerArray.join(" ");
+    console.log(count)
+    $('#counter').text('Total Number of guesses: ' + count);
+    $('#answer').text(answerArray.join(' '));
+    isGameWon()
   }
+
+  if(count > chosenWord.length) {
+    $('stat').html('You should have guessed it by now!')
+    alert('You should have guessed it by now!')
+  }
+}
+
+function isGameWon() {
+  if ( answerArray.join('') === chosenWord ){
+    alert('You have guessed the word, congrats!')
+  }
+}
+
+buttn1.on('click', startOver)
+
+function startOver(){
+  answerArray=[];
+  count=0;
+  var newWord = prompt("Player 1, choose a word for Player 2 to guess!")
+  begin();
+}
+
+
+// })
